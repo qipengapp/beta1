87de564977e6fdaddc3cdab367276bee5536dadd
snstli <snst@live.cn>
2017-07-23 09:08:55 +0800

    Colin Mg app.mod.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,28 +7,13 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { CompinfoPage } from '../pages/compinfo/compinfo';
 import { IndustrylistPage } from '../pages/industrylist/industrylist';
-
-=======
-=======
-import { CompinfoPage } from '../pages/compinfo/compinfo';
-import { IndustrylistPage } from '../pages/industrylist/industrylist';
-
->>>>>>> 41ea23516608c382193fd72825d66db9f8d21c03
 import { WelcomePage } from '../pages/welcome/welcome';
 import { LoginPage } from '../pages/login/login';
-<<<<<<< HEAD
 import { CodeBean } from '../pages/login/code.bean';
-=======
->>>>>>> f0e8302cae0da5500903182744fbabcff7cd1112
->>>>>>> 589fde3f0aa9d531eb63f58236e96e14fd78fbac
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { IonicStorageModule } from '@ionic/Storage';
 import { HttpService } from "../service/http.service";
 import { UserService } from "../service/user.service";
@@ -42,21 +27,12 @@ import { Http, Response, Headers, RequestOptions } from '@angular/http';
     AboutPage,
     ContactPage,
     HomePage,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    TabsPage,
-    CompinfoPage,
-    IndustrylistPage
-=======
-=======
     TabsPage,
     CompinfoPage,
     IndustrylistPage,
->>>>>>> 41ea23516608c382193fd72825d66db9f8d21c03
     WelcomePage,
     LoginPage,
     TabsPage
->>>>>>> f0e8302cae0da5500903182744fbabcff7cd1112
   ],
   imports: [
     BrowserModule,
@@ -70,21 +46,11 @@ import { Http, Response, Headers, RequestOptions } from '@angular/http';
     AboutPage,
     ContactPage,
     HomePage,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    TabsPage,
     CompinfoPage,
-    IndustrylistPage
-=======
-=======
-    TabsPage,
-    CompinfoPage,
-    IndustrylistPage
->>>>>>> 41ea23516608c382193fd72825d66db9f8d21c03
+    IndustrylistPage,
     WelcomePage,
     LoginPage,
     TabsPage
->>>>>>> f0e8302cae0da5500903182744fbabcff7cd1112
   ],
   providers: [
     StatusBar,
